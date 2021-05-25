7fbfa75162fa60aeb2e20372d484d11528b115ad
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-25 00:50:59 -0300

    Estilizacao com o SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Olá Dev!</h1>
+    <h1 className={styles.title}>Olá <span>Dev!</span></h1>
   )
 }
