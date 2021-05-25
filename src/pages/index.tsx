053a733d724d61a4b370175b9ddda04a9020b6b7
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-25 01:09:05 -0300

    Configuracao do ESLint, Editorconfig e Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Olá <span>Dev!</span></h1>
-  )
+    <h1 className={styles.title}>
+      Olá <span>Dev!</span>
+    </h1>
+  );
 }
