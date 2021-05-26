75644ace967433fea0768e976907691e74b5e6e1
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-26 00:59:42 -0300

    Client Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import styles from '../styles/home.module.scss';
+import { useEffect, useState } from 'react';
+
+interface Post {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/posts').then(response => {
+      response.json().then(data => {
+        setPosts(data);
+      });
+    });
+  }, []);
+
   return (
-    <h1>
-      Olá <span>Dev!</span>
-    </h1>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
