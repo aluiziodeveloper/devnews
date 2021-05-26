7f67bfb209f91f43e640458e04e1e059694507c5
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-26 00:04:14 -0300

    Componente Header - continuacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.container}>
       <div className={styles.content}>
         <img src="/logo.svg" alt="DevNews!" />
         <nav>
-          <a href="#">Home</a>
-          <a href="#">Posts</a>
+          <Link href="/">
+            <a className={asPath === '/' ? styles.active : ''}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
+          </Link>
         </nav>
       </div>
     </header>
