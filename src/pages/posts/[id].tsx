358666ba5377370222beecd7bbd777cf36d35d2a
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-28 00:59:57 -0300

    Static Site Generation - por parametro

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,12 +1,64 @@
-import {useRouter} from 'next/router'
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
-export default function Post() {
+interface Comment {
+  id: string;
+  body: string;
+}
+
+interface CommentsProps {
+  comments: Comment[];
+}
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Post({ comments }: CommentsProps) {
   const router = useRouter();
 
   return (
     <>
       <h1>Post {router.query.id}</h1>
-      <p>{router.asPath}</p>
+      <ul>
+        {comments.map(comment => (
+          <li key={comment.id}>{comment.body}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+
+  const paths = posts.map(post => {
+    return {
+      params: { id: String(post.id) },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<CommentsProps> = async context => {
+  const { id } = context.params;
+  const response = await fetch(`http://localhost:3333/comments?postId=${id}`);
+  const comments = await response.json();
+
+  return {
+    props: {
+      comments,
+    },
+    revalidate: 5, // In seconds
+  };
+};
