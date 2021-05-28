7b07954ba4470c20a61be4a4f59cf149a391b1c0
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-28 00:02:42 -0300

    Static Site Generation - parte 1

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,34 @@
-export default function Posts() {
-  return <h1>Posts</h1>;
+import { GetStaticProps } from 'next';
+
+interface Post {
+  id: string;
+  title: string;
 }
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <div>
+      <h1>Listagem de Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
