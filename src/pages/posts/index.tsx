6c733f2f9402740bc29bcb994ca755508c24974c
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-04 17:07:35 -0300

    Consumindo o Prismic API - parte 2

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 import Link from 'next/link';
 import SEO from '../../components/SEO';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './posts.module.scss';
 
 interface Post {
@@ -33,6 +35,17 @@ export default function Posts() {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+    },
+  );
+
+  console.log(response);
+
   return {
     props: {},
     revalidate: 60 * 60 * 12, // 12 horas
