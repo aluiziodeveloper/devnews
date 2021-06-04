803daf02249b5a73750652e055fe70b6bcdd0082
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-04 15:22:24 -0300

    Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,26 @@
-import { GetServerSideProps } from 'next';
 import SEO from '../components/SEO';
+import styles from '../styles/home.module.scss';
 
-interface Post {
-  id: string;
-  title: string;
-}
-
-interface HomeProps {
-  posts: Post[];
-}
-
-export default function Home({ posts }: HomeProps) {
+export default function Home() {
   return (
-    <div>
-      <SEO title="Home" />
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title="Dev News!" excludeTitleSuffix />
+
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev!</span>
+          <h1>
+            Bem-vindo e bem-vinda <br />
+            ao <span>Dev</span>News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br />
+            <span>relevantes para o seu aprendizado.</span>
+          </p>
+        </section>
+
+        <img src="/home.svg" alt="Home image" />
+      </main>
+    </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
