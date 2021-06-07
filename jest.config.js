fdf5cf0b1be85f0b3a727207efbc2f09ab562853
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-07 12:15:57 -0300

    Configurando o Jest e a Testing-library

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy"
+  }
+};
