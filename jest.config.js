40328d5b8afbb650d8ef0c36c9048534aa1098c6
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-07 14:16:58 -0300

    Testes do Componente ActiveLink

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
